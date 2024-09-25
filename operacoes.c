c3a7d552d0aed7694a2582c7d94e2d575d84bdaa
otaviobia <otaviobiagioni31@gmail.com>
2024-09-25 15:11:50 -0300

    Adicionei TAD

--- operacoes.c
+++ operacoes.c
@@ -6,29 +6,56 @@ struct item_ {
     float razao; 
 };
 
-//Implementação do algoritmo guloso
-void alg_guloso(ITEM *items, int qntd_items, int peso_limite) {
+ITEM *item_criar(int valor, int peso)
+{
+    ITEM *item = (ITEM *) malloc(sizeof(ITEM));
 
-    ITEM *items_aux = (ITEM*) malloc(sizeof(ITEM)*qntd_items); 
+    item->valor = valor;
+    item->peso = peso;
+
+    return(item);
+}
+
+void item_apagar(ITEM **item)
+{
+    free(*item);
+}
+
+void item_set_valor(ITEM *item, int valor)
+{
+    item->valor = valor;
+}
+
+void item_set_peso(ITEM *item, int peso)
+{
+    item->peso = peso;
+}
+
+void quick_sort(ITEM **itens, int inicio, int fim);
+
+//Implementação do algoritmo guloso
+void alg_guloso(ITEM **itens, int qntd_itens, int peso_limite) {
+
+    ITEM **itens_aux = (ITEM **) malloc(sizeof(ITEM *) * qntd_itens); 
     int soma_valores = 0; 
     int soma_pesos = 0; 
     int cont = 0; 
 
     // Copiando os items para o items_aux e calculando a razão valor/peso
-    for(int i =0; i<qntd_items; i++) {
-        items_aux[i] = items[i];
-        items_aux[i].razao = (float)items_aux[i].valor/items_aux[i].peso;
+    for(int i = 0; i < qntd_itens; i++) {
+        itens_aux[i] = itens[i];
+        itens_aux[i]->razao = (float)itens_aux[i]->valor/itens_aux[i]->peso;
     }
         
     // Ordenando em ordem decrescente 
-    quick_sort(items_aux, 0, qntd_items-1); 
+    quick_sort(itens_aux, 0, qntd_itens - 1); 
 
     // Calculando a soma total dos items na mochila     
-    for(int i =0; i<qntd_items; i++) {
-        if(items_aux[i].peso <= peso_limite) {
-            soma_valores += items_aux[i].valor; 
-            soma_pesos += items_aux[i].peso; 
-            peso_limite -= items_aux[i].peso; 
+    for(int i = 0; i < qntd_itens; i++) {
+        if(itens_aux[i]->peso <= peso_limite) {
+            soma_valores += itens_aux[i]->valor; 
+            soma_pesos += itens_aux[i]->peso; 
+            peso_limite -= itens_aux[i]->peso; 
         } 
         else 
             break; 
@@ -37,37 +64,77 @@ void alg_guloso(ITEM *items, int qntd_items, int peso_limite) {
     printf("Valor máximo na mochila: %d\n", soma_valores); 
     printf("Peso total: %d\n", soma_pesos); 
 
-    free(items_aux);
+    free(itens_aux);
 }
 
 // Algoritmo de ordenação quick_sort
-void quick_sort(ITEM *items, int inicio, int fim) {
+void quick_sort(ITEM **itens, int inicio, int fim) {
     
     if(inicio < fim) {
-        float pivo = items[(inicio+fim)/2].razao; 
+        float pivo = itens[(inicio+fim)/2]->razao; 
         int i, j; 
-        ITEM tmp; 
+        ITEM* tmp = (ITEM *) malloc(sizeof(ITEM));
 
         i = inicio; 
         j = fim; 
 
         do {
-            while(items[i].razao > pivo) 
+            while(itens[i]->razao > pivo) 
                 i++; 
-            while(items[j].razao < pivo) 
+            while(itens[j]->razao < pivo) 
                 j--; 
             
             if(j>=i) {
-                tmp = items[i]; 
-                items[i] = items[j]; 
-                items[j] = tmp; 
+                tmp = itens[i]; 
+                itens[i] = itens[j]; 
+                itens[j] = tmp; 
                 i++; 
                 j--; 
             }
 
         } while(i<j); 
 
-        quick_sort(items, inicio, j); 
-        quick_sort(items, i, fim); 
+        quick_sort(itens, inicio, j); 
+        quick_sort(itens, i, fim); 
     }
+}
+
+void forca_bruta(ITEM **itens, int qntd_itens, int peso_limite)
+{
+    int peso_total = 0;
+    int valor_total = 0;
+
+    int maior_valor = 0;
+    int peso_do_maior_valor = 0;
+
+    // 2^n combinações
+    int total_combinacoes = 1 << qntd_itens;
+
+    for (int i = 1; i < total_combinacoes; i++)
+    {
+        valor_total = 0;
+        peso_total = 0;
+
+        for (int j = 0; j < qntd_itens; j++) 
+        {
+            if (i & 1 << j) 
+            {
+                valor_total += itens[j]->valor;
+                peso_total += itens[j]->peso;
+            }
+        }
+
+        // verificar se está na condição (cabe na mochila?)
+        if (peso_total <= peso_limite)
+        {
+            if (valor_total > maior_valor)
+            {
+                maior_valor = valor_total;
+                peso_do_maior_valor = peso_total;
+            }
+        }
+    }
+
+    printf("Valor máximo na mochila: %d\n", maior_valor); 
+    printf("Peso total: %d\n", peso_do_maior_valor); 
 }
--- operacoes.h
+++ operacoes.h
@@ -6,5 +6,10 @@
 
 	typedef struct item_ ITEM;
 
-	void alg_guloso(ITEM *items, int qntd_items, int peso_limite);
+	ITEM *item_criar(int valor, int peso);
+	void item_apagar(ITEM **item);
+	void item_set_valor(ITEM *item, int valor);
+	void item_set_peso(ITEM *item, int peso);
+	void alg_guloso(ITEM **itens, int qntd_itens, int peso_limite);
+	void forca_bruta(ITEM **itens, int qntd_itens, int peso_limite);
 #endif
